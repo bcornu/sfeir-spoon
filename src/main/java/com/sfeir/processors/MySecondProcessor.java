e0beae8ceff254b79660f99912109a00665fb524
bcornu <cornu.b@sfeir.com>
2017-02-07 16:55:41 +0100

    v2.0

---
+++ src/main/java/com/sfeir/processors/MySecondProcessor.java
@@ -0,0 +1,15 @@
+package com.sfeir.processors;
+
+import spoon.processing.AbstractProcessor;
+import spoon.reflect.code.*;
+
+/**
+ * Created by bcornu on 2/2/17.
+ */
+public class MySecondProcessor extends AbstractProcessor<CtIf> {
+
+    public void process(CtIf element) {
+        //NYI
+    }
+
+}
