4e116217ac12beb48a7180598577daed377c11a8
bcornu <cornu.b@sfeir.com>
2017-02-07 16:57:54 +0100

    v3

--- src/main/java/com/sfeir/processors/MySecondProcessor.java
+++ src/main/java/com/sfeir/processors/MySecondProcessor.java
@@ -9,7 +9,19 @@ import spoon.reflect.code.*;
 public class MySecondProcessor extends AbstractProcessor<CtIf> {
 
     public void process(CtIf element) {
-        //NYI
+        if (element.getCondition() instanceof CtBinaryOperator) {
+            CtBinaryOperator op = (CtBinaryOperator) element.getCondition();
+            if (op.getKind().equals(BinaryOperatorKind.EQ)) {
+                CtExpression left = op.getLeftHandOperand();
+                CtExpression right = op.getRightHandOperand();
+                if (left.getType().getActualClass().equals(String.class)
+                        && right.getType().getActualClass().equals(String.class)) {
+                    System.out.print("String compare with == @");
+                    System.out.println(element.getPosition());
+                    System.out.println(element.getCondition());
+                }
+            }
+        }
     }
 
 }
