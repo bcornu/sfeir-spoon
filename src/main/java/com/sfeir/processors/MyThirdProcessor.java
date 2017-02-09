94b0f37781b390b650dc83093c783dc963b7d00d
bcornu <cornu.b@sfeir.com>
2017-02-09 10:34:23 +0100

    v5

--- src/main/java/com/sfeir/processors/MyThirdProcessor.java
+++ src/main/java/com/sfeir/processors/MyThirdProcessor.java
@@ -3,6 +3,11 @@ package com.sfeir.processors;
 import spoon.processing.AbstractProcessor;
 import spoon.reflect.code.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by bcornu on 2/2/17.
  */
@@ -13,15 +18,42 @@ public class MyThirdProcessor extends AbstractProcessor<CtIf> {
         if (element.getParent(CtIf.class) == null) {//pas dans un CtIf
             if (isStringEqualsCompare(element)){//condition de la forme String#equals
                 if(hasSameElseChildren(element)){//idem pour le else
-                    System.out.println("CANDIDAT !!!");
+
+                    Map<CtExpression, CtBlock> cases = getCases(element);
+
+                    List<CtCase> ctCases = new ArrayList<>();
+                    for (CtExpression expr : cases.keySet()) {
+                        CtCase ctCase = getFactory().Core().createCase();
+                        ctCase.setCaseExpression(expr);
+                        ctCase.setStatements(cases.get(expr).getStatements());
+                        ctCases.add(ctCase);
+                    }
+
+                    CtSwitch newSwitch = getFactory().Core().createSwitch();
+                    newSwitch.setCases(ctCases);
+                    newSwitch.setSelector(((CtInvocation)element.getCondition()).getTarget());
+
+                    element.replace(newSwitch);
                 }
             }
         }
-        System.out.println("nope");
+    }
+
+    private Map<CtExpression, CtBlock> getCases(CtIf element) {
+        CtBlock elseBlock = element.getElseStatement();
+        if(elseBlock==null){
+            return new HashMap<>();
+        }
+        CtIf childrenIf = (CtIf)elseBlock.getLastStatement();
+        Map<CtExpression, CtBlock> res = getCases(childrenIf);
+        CtInvocation op = (CtInvocation) element.getCondition();
+        CtExpression target = (CtExpression) op.getArguments().get(0);
+        res.put(target, (CtBlock) element.getThenStatement());
+        return res;
     }
 
     private boolean hasSameElseChildren(CtIf element) {
-        CtBlock elseBlock = ((CtBlock)element.getElseStatement());
+        CtBlock elseBlock = element.getElseStatement();
         if(elseBlock==null){//pas de else donc OK
             return true;
         }
